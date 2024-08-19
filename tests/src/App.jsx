7afa8838a17d2af006c91fbbc1a2147d1495ae00
Jia Wang <amber.jiawang@gmail.com>
2024-08-19 21:12:58 +0200

    part1 c complex state

--- tests/src/App.jsx
+++ tests/src/App.jsx
@@ -1,36 +1,30 @@
 import { useState } from "react";
 
-const Display = ({counter}) => <div>{counter}</div>
-
-const Button = ({ onClick, text }) =>  <button onClick={onClick}>{text}</button>
-
 const App = () => {
-  const [counter, setCounter] = useState(0);
-  console.log("render with counter value", counter);
+  const [clicks, SetClicks] = useState( 
+    {left: 0, right: 0}
+  )
 
-  const increaseByOne = () => {
-    console.log("increase, value before", counter);
-    setCounter(counter + 1);
-  };
+  const handleLeftClick = () => SetClicks({
+      ...clicks,
+      left: clicks.left + 1,
+    })
+    
 
-  const descreaseByOne = () => {
-    console.log("descrese, value before", counter);
-    setCounter(counter - 1);
-  };
-
-  const setToZero = () => {
-    console.log("resetting to zero, value before", counter);
-    setCounter(0);
-  };
+  const handleRightClicks = () => SetClicks({
+    ...clicks,
+    right: clicks.right + 1})
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text="plus" />
-      <Button onClick={descreaseByOne} text="minus" />
-      <Button onClick={setToZero} text="zero" />
+      {clicks. left}
+      <button onClick={handleLeftClick}>
+      left
+      </button>
+      <button onClick={handleRightClicks}>right</button>
+      {clicks.right}
     </div>
-  );
-};
+  )
+}
 
 export default App;
