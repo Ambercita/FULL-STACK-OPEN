b767491d6de98ea8bc6668adf67522d9f9ddadfe
Jia Wang <amber.jiawang@gmail.com>
2024-09-08 21:02:42 +0200

    practiced conditioning rendering
    and updat of the state is asynchronous. which belongs to part 1. d

--- tests/src/App.jsx
+++ tests/src/App.jsx
@@ -1,28 +1,58 @@
 import { useState } from "react";
 
-const App = () => {
-  const [clicks, SetClicks] = useState( 
-    {left: 0, right: 0}
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.allClicks.join (' ')}
+    </div>
   )
+}
 
-  const handleLeftClick = () => SetClicks({
-      ...clicks,
-      left: clicks.left + 1,
-    })
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
+
+
+  const handleLeftClick = () => {
+      setAll(allClicks.concat('L'))
+      console.log('left before', left)
+      const updatedLeft = left + 1
+      console.log('left after', updatedLeft)
+      setLeft(updatedLeft)
+      setTotal(updatedLeft + right)
+    }
     
 
-  const handleRightClicks = () => SetClicks({
-    ...clicks,
-    right: clicks.right + 1})
+  const handleRightClicks = () => {
+    setAll(allClicks.concat('R'))
+    const updatedRight = right + 1
+    setRight(updatedRight)
+    setTotal(left + updatedRight)
+  }
 
   return (
     <div>
-      {clicks. left}
-      <button onClick={handleLeftClick}>
-      left
-      </button>
-      <button onClick={handleRightClicks}>right</button>
-      {clicks.right}
+    {left}
+    <Button handleClick={handleLeftClick} text='left' />
+    <Button handleClick={handleRightClicks} text='right' />
+    {right}
+    <History  allClicks={allClicks}/>
     </div>
   )
 }
